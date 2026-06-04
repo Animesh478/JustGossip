753350c8b7b0cce95ff0a0fbdeb4fbbdc072b6ab
Animesh478 <animeshmohanty.am@gmail.com>
2026-06-04 15:50:44 +0530

    sign up functionality

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,11 +1,27 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import apiClient from "../api/axios";
 
 function SignUp() {
+  const navigate = useNavigate();
+  const handleSignup = async function (formData) {
+    const userDetails = {
+      username: formData.get("username"),
+      email: formData.get("email"),
+      password: formData.get("password"),
+      phoneNumber: formData.get("phoneNumber"),
+    };
+    try {
+      await apiClient.post("/user-auth/signup", userDetails);
+      navigate("/login");
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <div className="bg-primary-light h-screen flex  items-center justify-center">
       <div className="flex flex-col gap-8 items-center justify-between bg-stone-100 border-2 border-primary rounded-md px-4 py-6 font-poppins md:w-1/3">
         <h1 className="font-semibold text-4xl text-primary">JustGossip</h1>
-        <form className="flex flex-col gap-3 w-full py-4">
+        <form className="flex flex-col gap-3 w-full py-4" action={handleSignup}>
           <div className="flex gap-2 items-center justify-between">
             <label htmlFor="" className=" text-xl">
               Name
@@ -13,6 +29,7 @@ function SignUp() {
             <input
               type="text"
               className="border border-secondary rounded-md  px-2 py-1 focus:border-primary outline-none w-3/4"
+              name="username"
             />
           </div>
           <div className="flex gap-2 items-center justify-between">
@@ -22,6 +39,7 @@ function SignUp() {
             <input
               type="email"
               className="border border-secondary rounded-md  px-2 py-1 focus:border-primary outline-none w-3/4"
+              name="email"
             />
           </div>
           <div className="flex gap-2 items-center justify-between">
@@ -31,6 +49,7 @@ function SignUp() {
             <input
               type="text"
               className="border border-secondary rounded-md  px-2 py-1 focus:border-primary outline-none w-3/4"
+              name="phoneNumber"
             />
           </div>
           <div className="flex gap-2 items-center justify-between">
@@ -40,6 +59,7 @@ function SignUp() {
             <input
               type="password"
               className="border border-secondary rounded-md  px-2 py-1 focus:border-primary outline-none w-3/4"
+              name="password"
             />
           </div>
           <div className="mt-4">
